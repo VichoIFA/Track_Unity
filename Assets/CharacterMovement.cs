3606b672c52c7f6a929f847745fab3d8d6880c55
Vicente Fuentes <vicentedelaselva@gmail.com>
2024-08-26 14:46:00 -0400

    Creacion de proyecto

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+    
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            dir += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            dir -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir += transform.right;
+        }
+
+        dir = dir.normalized;
+
+        rb.AddForce(dir * speed * Time.deltaTime);
+    }
+}
