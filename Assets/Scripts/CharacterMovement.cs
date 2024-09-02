b2e2eab7cfad6f6a1e6f4ed98ce209ba95cb822c
InZenFenix <108585945+inzenfenix@users.noreply.github.com>
2024-09-01 21:28:31 -0400

    Movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class CharacterMovement : MonoBehaviour
 {
     
     [SerializeField] private float speed;
+    [SerializeField] private float cameraSpeed = 5f;
     private Rigidbody rb;
+
+    [SerializeField] private Transform characterCamera;
+
+    private float maxVerticalMovement = 45;
+
+    Vector2 cameraRotation = Vector2.zero;
 
     private void Awake()
     {
@@ -16,11 +24,17 @@ public class CharacterMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (rb.velocity.magnitude > 5f) return;
+        CharacterMovementKeyboard();
+        CameraMovement();
+    }
+
+    private void CharacterMovementKeyboard()
+    {
+        if (rb.velocity.magnitude > 4f) return;
 
         Vector3 dir = Vector3.zero;
 
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             dir += transform.forward;
         }
@@ -39,6 +53,31 @@ public class CharacterMovement : MonoBehaviour
 
         dir = dir.normalized;
 
-        rb.AddForce(dir * speed * Time.fixedDeltaTime);
+        rb.AddForce(dir * speed * 100 * Time.fixedDeltaTime);
+    }
+
+    private void CameraMovement()
+    {
+        Vector2 cameraDir = Mouse.current.delta.value;
+
+        if (Time.time < .1f) return;
+
+        cameraRotation.y += cameraDir.x * Time.fixedDeltaTime * cameraSpeed;
+        cameraRotation.x += -cameraDir.y * Time.fixedDeltaTime * cameraSpeed;
+
+        cameraRotation.x = Mathf.Clamp(cameraRotation.x, -maxVerticalMovement, maxVerticalMovement);
+        
+        if(Mathf.Abs(cameraRotation.y) >= 360)
+        {
+            cameraRotation.x = 0;
+        }
+
+        Vector2 cameraRot = new Vector2(cameraRotation.x, 0);
+
+        characterCamera.localEulerAngles = cameraRot;
+
+        Vector3 characterRot = new Vector2(0, cameraRotation.y);
+
+        transform.localEulerAngles = characterRot;
     }
 }
